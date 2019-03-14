bbecf79bafe91815505a78f8ea805e8eaf24dec7
Boa Matule <45321699+SnailCoder1@users.noreply.github.com>
2019-03-14 17:30:02 +0100

    Update name.rb
    Variables exercices.

---
+++ name.rb
@@ -0,0 +1,8 @@
+2.3.4 :010 > puts "Please, tape your name:"
+Please, tape your name:
+ => nil 
+2.3.4 :011 > name = gets.chomp
+Boa
+ => "Boa" 
+2.3.4 :012 > puts "Hello, #{name}! Welcome to the Craft Academy. The home crafting brains into tech geeks!" 
+Hello, Boa! Welcome to the Craft Academy. The home crafting brains into tech geeks!
